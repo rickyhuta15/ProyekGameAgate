200a30dec5b84856e74c42f7fd2afd4824067e07
rickyhuta15 <rickyhutagalung89@gmail.com>
2021-09-11 13:16:31 +0700

    commit

---
+++ DiloIncrementalGame/Assets/Scripts/AchievementController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+ 
+
+public class AchievementController : MonoBehaviour
+{
+    public AudioSource AudioAchievement;
+    private static AchievementController _instance = null;
+    public static AchievementController Instance
+   {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AchievementController> ();
+            }
+            return _instance;
+        }
+    }
+
+    [SerializeField] private Transform _popUpTransform;
+    [SerializeField] private Text _popUpText;
+    [SerializeField] private float _popUpShowDuration = 3f;
+    [SerializeField] private List<AchievementData> _achievementList; 
+    private float _popUpShowDurationCounter;
+ 
+
+    private void Update ()
+    {
+        if (_popUpShowDurationCounter > 0)
+        {         
+            _popUpShowDurationCounter -= Time.unscaledDeltaTime;
+            _popUpTransform.localScale = Vector3.LerpUnclamped (_popUpTransform.localScale, Vector3.one, 0.5f);
+        }
+        else
+        {           
+            _popUpTransform.localScale = Vector2.LerpUnclamped (_popUpTransform.localScale, Vector3.right, 0.5f);
+        }
+    }
+
+ 
+
+    public void UnlockAchievement (AchievementType type, string value)
+    {
+        AchievementData achievement = _achievementList.Find (a => a.Type == type && a.Value == value);
+        if (achievement != null && !achievement.IsUnlocked)
+        {  
+            achievement.IsUnlocked = true;
+            ShowAchivementPopUp (achievement);
+        }
+
+    }
+
+ 
+
+    private void ShowAchivementPopUp (AchievementData achievement)
+
+    {
+        AudioAchievement.Play();
+        _popUpText.text = achievement.Title;
+        _popUpShowDurationCounter = _popUpShowDuration;
+        _popUpTransform.localScale = Vector2.right;
+    
+
+    }
+
+}
+
+ 
+
+
+[System.Serializable]
+
+public class AchievementData
+{
+    public string Title;
+    public AchievementType Type;
+    public string Value;
+    public bool IsUnlocked;
+
+}
+
+ 
+
+public enum AchievementType
+{
+    UnlockResource
+}
---
+++ DiloIncrementalGame/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public AudioSource AudioUpgrade;
+    public AudioSource AudioFailUpgrade;
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+    public Button ResourceButton;
+    public Image ResourceImage;
+    private ResourceConfig _config;
+    private int _level = 1;
+    public bool IsUnlocked { get; private set; }
+    private void Start ()
+    {
+        ResourceButton.onClick.AddListener (() =>
+        {
+            if (IsUnlocked)
+            {
+                UpgradeLevel ();
+            }
+
+            else
+            {
+                UnlockResource ();
+            }
+        });
+        ResourceButton.onClick.AddListener (UpgradeLevel);
+    }
+
+    public void SetConfig (ResourceConfig config)
+    {
+        _config = config;
+        ResourceDescription.text = $"{ _config.Name } LV. { _level }\n+{ GetOutput ().ToString ("0") }";
+        ResourceUnlockCost.text = $"BIAYA MEMBUKA\n{ _config.UnlockCost }";
+        ResourceUpgradeCost.text = $"BIAYA PENINGKATAN\n{ GetUpgradeCost () }";
+        SetUnlocked (_config.UnlockCost == 0);
+    }
+
+    public double GetOutput ()
+    {
+        return _config.Output * _level;
+    }
+
+ 
+
+    public double GetUpgradeCost ()
+    {
+        return _config.UpgradeCost * _level;
+    }
+
+ 
+
+    public double GetUnlockCost ()
+    {
+        return _config.UnlockCost;
+    }
+
+     public void UpgradeLevel ()
+    {
+        double upgradeCost = GetUpgradeCost ();
+        if (GameManager.Instance._totalGold < upgradeCost)
+        {
+           return;
+        }
+
+
+
+        GameManager.Instance.AddGold (-upgradeCost);
+
+         _level++;
+        AudioUpgrade.Play();
+
+ 
+
+        ResourceUpgradeCost.text = $"BIAYA PENINGKATAN\n{ GetUpgradeCost () }";
+
+        ResourceDescription.text = $"{ _config.Name } LV. { _level }\n+{ GetOutput ().ToString ("0") }";
+
+         }
+
+   
+
+    public void UnlockResource ()
+    {
+        double unlockCost = GetUnlockCost ();
+        if (GameManager.Instance._totalGold < unlockCost)
+        {
+            return;
+        }
+
+ 
+
+        SetUnlocked (true);
+
+        GameManager.Instance.ShowNextResource ();
+
+        AchievementController.Instance.UnlockAchievement (AchievementType.UnlockResource, _config.Name);
+
+    }
+
+ 
+
+    public void SetUnlocked (bool unlocked)
+    {
+        IsUnlocked = unlocked;
+        ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
+        ResourceUnlockCost.gameObject.SetActive (!unlocked);
+        ResourceUpgradeCost.gameObject.SetActive (unlocked);
+    }
+
+}
